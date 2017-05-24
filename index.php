9efa1f5b6c77ee787911bc82a430bae14f7e4c83
davidmwhynot <davidmwhynot@gmail.com>
2017-05-24 14:14:59 -0400

    changed images

--- index.php
+++ index.php
@@ -21,19 +21,6 @@
     </div>
   </nav>
   <div class="container" id="main">
-    <div class="top-images">
-      <div class="row">
-        <div class="col-md-4">
-          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
-        </div>
-        <div class="col-md-4">
-          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
-        </div>
-        <div class="col-md-4">
-          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
-        </div>
-      </div>
-    </div>
     <div class="tagline">
       <div class="row">
         <div class="col-md-6">
@@ -51,55 +38,65 @@
         </div>
       </div>
     </div>
+    <div class="box2">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+        </div>
+        <div class="col-md-4">
+          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+        </div>
+        <div class="col-md-4">
+          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+        </div>
+      </div>
+    </div>
     <div class="box1">
       <div class="row">
         <div class="col-md-4">
-          <h3 class="text-primary">Mythic Plus</h3>
           <div class="panel panel-default">
+            <div class="panel panel-heading">
+              <h3 class="panel-title text-primary">Mythic Plus</h3>
+            </div>
             <div class="panel-body">
-              <img src="img/serpentrix_model.jpg" alt="Mythic Plus">
+              Our mythic plus services include:
+              <ul>
+                <li>Guarunteed +3 chest</li>
+                <li>900-925 gear</li>
+                <li>Keystone Master Achievement</li>
+              </ul>
             </div>
           </div>
-          <p>
-            Our mythic plus services include:
-            <ul>
-              <li>Guarunteed +3 chest</li>
-              <li>900-925 gear</li>
-              <li>Keystone Master Achievement</li>
-            </ul>
-          </p>
         </div>
         <div class="col-md-4">
-          <h3 class="text-primary">Raids</h3>
           <div class="panel panel-default">
+            <div class="panel panel-heading">
+              <h3 class="panel-title text-primary">Raids</h3>
+            </div>
             <div class="panel-body">
-              <img src="img/serpentrix_model.jpg" alt="Mythic Plus">
+                We offer premium raid services:
+                <ul>
+                  <li>Tomb of Sargeras Pre-orders</li>
+                  <li>Heroic Gul'dan</li>
+                  <li>Nighthold Mythic</li>
+                </ul>
             </div>
           </div>
-          <p>
-            We offer premium raid services:
-            <ul>
-              <li>Tomb of Sargeras Pre-orders</li>
-              <li>Heroic Gul'dan</li>
-              <li>Nighthold Mythic</li>
-            </ul>
-          </p>
         </div>
         <div class="col-md-4">
-          <h3 class="text-primary">Power Leveling</h3>
           <div class="panel panel-default">
+            <div class="panel panel-heading">
+              <h3 class="panel-title text-primary">Power Leveling</h3>
+            </div>
             <div class="panel-body">
-              <img src="img/serpentrix_model.jpg" alt="Mythic Plus">
+              Get your character boosted today:
+              <ul>
+                <li>iLvl 860 level 110 characters</li>
+                <li>1-100 in < 3 days</li>
+                <li>100-110 in < 24 hours</li>
+              </ul>
             </div>
           </div>
-          <p>
-            Get your character boosted today:
-            <ul>
-              <li>iLvl 860 level 110 characters</li>
-              <li>1-100 in < 3 days</li>
-              <li>100-110 in < 24 hours</li>
-            </ul>
-          </p>
         </div>
       </div>
     </div>
