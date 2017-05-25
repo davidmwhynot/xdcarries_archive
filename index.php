4d637702790322c0f8121b82f9bc2c07e6037f85
David Whynot <davidmwhynot@gmail.com>
2017-05-25 16:00:43 -0400

    new images and updated template and css

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include "inc/header.php"; ?>
 <body>
-  <nav class="navbar navbar-inverse">
+  <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -9,7 +9,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">XDCarries</a>
+        <a class="navbar-brand" href="index.php">XDCarries</a>
       </div>
       <div id="navbar" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
@@ -23,8 +23,8 @@
   <div class="container" id="main">
     <div class="tagline">
       <div class="row">
-        <div class="col-md-6">
-          <h1><span class="text-primary">XDCarries</span>
+        <div class="col-sm-6">
+          <h1><span class="text-primary">XD</span>Carries
             <small class="text-warning">The premium WoW experience</small>
           </h1>
           <p class="lead">
@@ -33,25 +33,93 @@
             the best experience possible.
           </p>
         </div>
-        <div class="col-md-6">
-          <img src="img/wowlogo_516x411.png" alt="World of Warcraft: Legion">
+        <div class="col-sm-6 text-center">
+          <img src="img/wowlogo_482x246.png" alt="World of Warcraft: Legion">
+        </div>
+      </div>
+    </div>
+    <div class="box1">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+          <button type="button" class="btn-success block-button">Shop Now</button>
+        </div>
+        <div class="col-md-4">
+          <img src="img/power_leveling_1080x1080.jpg" alt="Power Leveling">
+          <button type="button" class="btn-success block-button">Shop Now</button>
+        </div>
+        <div class="col-md-4">
+          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+          <button type="button" class="btn-success block-button">Shop Now</button>
         </div>
       </div>
     </div>
     <div class="box2">
       <div class="row">
         <div class="col-md-4">
-          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+          <div class="panel panel-default">
+            <div class="panel panel-heading">
+              <h3 class="panel-title text-primary">Mythic Plus</h3>
+            </div>
+            <div class="panel-body">
+              Our mythic plus services include:
+              <ul>
+                <li>Guarunteed +3 chest</li>
+                <li>900-925 gear</li>
+                <li>Keystone Master Achievement</li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div class="col-md-4">
-          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+          <div class="panel panel-default">
+            <div class="panel panel-heading">
+              <h3 class="panel-title text-primary">Raids</h3>
+            </div>
+            <div class="panel-body">
+                We offer premium raid services:
+                <ul>
+                  <li>Tomb of Sargeras Pre-orders</li>
+                  <li>Heroic Gul'dan</li>
+                  <li>Nighthold Mythic</li>
+                </ul>
+            </div>
+          </div>
         </div>
         <div class="col-md-4">
-          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+          <div class="panel panel-default">
+            <div class="panel panel-heading">
+              <h3 class="panel-title text-primary">Power Leveling</h3>
+            </div>
+            <div class="panel-body">
+              Get your character boosted today:
+              <ul>
+                <li>iLvl 860 level 110 characters</li>
+                <li>1-100 in < 3 days</li>
+                <li>100-110 in < 24 hours</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <div class="box1">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+          <button type="button" class="btn-success block-button">Shop Now</button>
+        </div>
+        <div class="col-md-4">
+          <img src="img/power_leveling_1080x1080.jpg" alt="Power Leveling">
+          <button type="button" class="btn-success block-button">Shop Now</button>
+        </div>
+        <div class="col-md-4">
+          <img src="img/karazhan_1080x1080.jpg" alt="Karazhan">
+          <button type="button" class="btn-success block-button">Shop Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="box2">
       <div class="row">
         <div class="col-md-4">
           <div class="panel panel-default">
