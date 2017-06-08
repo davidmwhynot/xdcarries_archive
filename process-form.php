938e72cefc79e42d2f7b5f75f1738cc80ad3c5b4
davidmwhynot <davidmwhynot@gmail.com>
2017-06-08 08:00:34 -0400

    updated form processor

--- process-form.php
+++ process-form.php
@@ -1,11 +1,14 @@
 <?php
 // check for submit
+function processContact() {
   if(filter_has_var(INPUT_POST, 'submit')) {
     // get form data
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+    $name = htmlspecialchars($_POST['contact-name']);
+    $email = htmlspecialchars($_POST['contact-email']);
+    $message = htmlspecialchars($_POST['contact-message']);
+  }
 }
+// redirect function
 function redirect($url, $statusCode = 303) {
  header('Location: ' . $url, true, $statusCode);
  die();
